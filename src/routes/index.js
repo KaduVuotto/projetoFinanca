8b6bb305f31f4c426e118b3944b9ac689ac55178
CarlosAmorim <carlos.amorim@rckyinformatica.onmicrosoft.com>
2021-01-05 19:37:31 -0300

    Logando usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,19 @@ import { AuthContext } from '../contexts/auth'
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
+import { ActivityIndicator, View, Text } from "react-native";
 
 function Routes() {
-    const { signed } = useContext(AuthContext);
+    const { signed, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#049be3' }}>
+                <ActivityIndicator size='large' color='#0464ca' />
+                <Text style={{ color: '#0464ca' }}>Carregando, aguarde...</Text>
+            </View>
+        )
+    }
 
     return (
         signed ? <AppRoutes /> : <AuthRoutes />
